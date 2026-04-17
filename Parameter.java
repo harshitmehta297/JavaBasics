28d98f17cc3321c996fcbb5af907c7e642755247
harshitmehta297 <harshitmehta297@gmail.com>
2026-04-17 16:12:59 +0530

    function Parameter example

---
+++ Parameter.java
@@ -0,0 +1,11 @@
+public class Parameter {
+    static void main() {
+        int num = sumTwoNumbers(5,10);
+        System.out.println(num);
+    }
+
+    public static int sumTwoNumbers(int first, int second){
+        int sum = first + second;
+        return sum;
+    }
+}
