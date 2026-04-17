0331b366b0d3468d95c9cd2ceddd53ebf738d6fd
harshitmehta297 <harshitmehta297@gmail.com>
2026-04-17 16:14:31 +0530

    Program to add all odd no. till N

--- FunctionPractise.java
+++ FunctionPractise.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class FunctionPractise {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter no.: ");
+        int num = input.nextInt();
+        int sum = oddNumSum(num);
+        System.out.println("Sum is: " + sum);
+    }
+
+    public static int oddNumSum(int num){
+        int sum = 0;
+        int i = 1;
+        while ( i <= num){
+            sum = sum + i;
+            i = i+2;
+
+        }
+        return sum;
+    }
 }
