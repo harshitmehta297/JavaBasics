5faab027153345f84a6fdc519f580d8b7bc63200
harshitmehta297 <harshitmehta297@gmail.com>
2026-04-23 21:00:33 +0530

    Program to reverse a no.

--- ReverseANumber.java
+++ ReverseANumber.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ReverseANumber {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter No.: ");
+        int num = input.nextInt();
+        int reverse = reverseNo(num);
+        System.out.println("Reverse of your no. is: " + reverse);
+
+    }
+
+    public static int reverseNo(int num){
+        int newNum = 0;
+        while (num > 0){
+            int digit = num % 10;
+            newNum = newNum * 10 + digit;
+            num = num / 10;
+        }
+        return newNum;
+
+    }
 }
